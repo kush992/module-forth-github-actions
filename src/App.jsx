4b54fdd06c38929cc05a47ebc686895e27fca98a
Kush <85061303+kush992@users.noreply.github.com>
2024-03-10 22:36:31 +0100

    updated content app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import MainContent from './components/MainContent';
 import logo from './assets/images/logo.png';
 
 function App() {
-  return (
-    <>
-      <header>
-        <div id="logo-img">
-          <img src={logo} />
-        </div>
-        <h1>Learn & Master GitHub Actions</h1>
-      </header>
-      <MainContent />
-    </>
-  );
+	return (
+		<>
+			<header>
+				<div id='logo-img'>
+					<img src={logo} />
+				</div>
+				<h1>Learn & Master GitHub Actions</h1>
+				<p>Trigger Pull Request Workflow</p>
+			</header>
+			<MainContent />
+		</>
+	);
 }
 
 export default App;
